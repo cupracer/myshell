125d06bfd43aad969be35c64c2bd5b3d6271c645
Thomas Schulte <thomas@cupracer.de>
2017-01-06 10:52:20 +0100

    added script to convert uptime seconds in dmesg output of SUSE's supportconfig archives to absolute timestamps

---
+++ scripts/supportconfig-dmesg-timestamps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+	BASEDIR="$1"
+else
+	BASEDIR="."
+fi
+
+MISSINGFILES=0
+
+if ! [ -r "${BASEDIR}/boot.txt" ]; then
+	echo "* missing \"${BASEDIR}/boot.txt\""
+	MISSINGFILES=1
+fi
+
+if ! [ -r "${BASEDIR}/basic-environment.txt" ]; then
+	echo "* missing \"${BASEDIR}/basic-environment.txt\""
+	MISSINGFILES=1
+fi
+
+if ! [ -r "${BASEDIR}/proc.txt" ]; then
+	echo "* missing \"${BASEDIR}/proc.txt\""
+	MISSINGFILES=1
+fi
+
+if [ $MISSINGFILES -ne 0 ]; then
+	echo "aborting"
+	exit 1
+fi
+
+sed -n "/\#\ \/bin\/dmesg/,\$p" "${BASEDIR}/boot.txt" | sed "1d" | \
+	perl -ne "BEGIN{\$a= $(date --date="$(cat "${BASEDIR}/basic-environment.txt" |grep -A1 /bin/date |grep -v date)" +\"%s\")- \
+		qx!$(grep -A1 \"/proc/uptime\" "${BASEDIR}/proc.txt" |grep -v uptime)!}; s/\[\s*(\d+)\.\d+\]/localtime(\$1 + \$a)/e; print \$_;"
+
